57b604cc1a77964e42edad7450c9fd5ea1ff4abd
francescopanico <frpanico96@gmail.com>
2024-05-19 12:32:45 +0200

    [DjangoRecipeApi] tdd example reformatting file

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,25 +6,25 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-  """
+    """
   Test the calc module.
   """
 
-  def test_add_numbers(self):
-    """
+    def test_add_numbers(self):
+        """
     Test adding numbers together
     """
-    res = calc.add(5, 6)
+        res = calc.add(5, 6)
 
-    self.assertEqual(res, 11, "Add function wrong")
-  
+        self.assertEqual(res, 11, "Add function wrong")
 
-  def test_subtract_numbers(self):
-    """
+    def test_subtract_numbers(self):
+        """
     Test subtracting numbers.
     """
 
-    res = calc.subtract(15, 10)
+        res = calc.subtract(15, 10)
 
-    self.assertEqual(res, 5, "Subtraction function wrong")
+        self.assertEqual(res, 5, "Subtraction function wrong")
